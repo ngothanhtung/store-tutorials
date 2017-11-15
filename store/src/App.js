d7731875b48421ea4b019b6dcba83fd0d54e6447
Ngo Thanh Tung <tungnt@softech.vn>
2017-11-15 20:29:06 +0700

    Update Store

--- store/src/App.js
+++ store/src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Footer from './Components/Footer';
+import MainMenu from './Components/MainMenu';
+
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom'
+
+import Home from './pages/Home/index';
+import About from './pages/About/index';
+import Login from './pages/Login/index';
+import Products from './pages/Products/Index';
+import ProductDetail from './pages/Products/Detail';
 import './App.css';
-import LoginForm from './LoginForm';
-import UserList from './UserList';
-import ProductList from './ProductList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">        
-        <div>          
-          <ProductList />
-          <hr />
-          <LoginForm />
-          <hr />
-          <UserList />
-         
-         
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route path="/:id?" component={PageRoute}/>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/products" component={Products}/>
+                    <Route path="/product/:id" component={ProductDetailRoute}/>
+                    <Route path="/login" component={LoginRoute}/>
+                    <Route path="/about" component={About}/>
+                    <Footer/>
+                </div>
+
+            </Router>
+        );
+    }
 }
+
+const ProductDetailRoute = ({match}) => (
+    <div>
+        <ProductDetail id={match.params.id}/>
+    </div>
+)
+
+const PageRoute = ({match}) => (
+    <div>
+        <MainMenu activeItem={match.params.id != null ? match.params.id : "home"}/>
+    </div>
+)
+
+const LoginRoute = () => (
+    <div>
+        <Login title="Hello" />
+    </div>
+)
 
 export default App;
